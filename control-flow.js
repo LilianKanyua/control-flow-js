ba5edc8365bf6ca5bfe91860b6e9a993eb0c6a96
LilianKanyua <liliankanyua@gmail.com>
2023-04-05 07:32:26 +0300

    commit

--- control-flow.js
+++ control-flow.js
@@ -1,32 +1,50 @@
 //Question one
-let arrStrings=["my","happy","place"];
-for(let i=0;i<arrStrings.length;i++){
+
+function printElement(arrStrings){
+
+   for(let i=0;i<arrStrings.length;i++){
 console.log(arrStrings[i]);
 }
+}
+let arrStrings=["my","happy","place"];
+printElement(arrStrings);
 
 //Question two
-let arrNum1=[33,12,45,67,90];
-arrNum1.forEach(element =>{
-    console.log(element*2);
+
+function multiply(arrNum1){
+
+   arrNum1.forEach(element =>{
+console.log(element*2);
 })
+}
+let arrNum1=[33,12,45,67,90];
+multiply(arrNum1);
 
 //Question three
 //Question four
 
-let arrNum=[1,2,3,4,5,6,7,8,9];
+function breakAtFour(arrNum){
 for(let i=0; i<arrNum.length;i++){
     if(i==5){
         break;
     }
     console.log({"num":arrNum[i]});
     }
+}
+    let arrNum=[1,2,3,4,5,6,7,8,9];
+    breakAtFour(arrNum);
 
     //Question five
-    let fruits= ['apple','plum','banana','strawberries','kiwi'];
-    for(let i=0; i<fruits.length;i++){
+  
+    function fruits(arrFruits){
+    
+    for(let i=0; i<arrFruits.length;i++){
         if(i==2){
             continue;
         }
-        console.log({"continue":fruits[i]});
+        console.log({"continue":arrFruits[i]});
         }
 
+    }
+    let arrFruits= ['apple','plum','banana','strawberries','kiwi'];
+    fruits(arrFruits);
